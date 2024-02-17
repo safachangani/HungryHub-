95af0026023d10e1f342d73bee3c19570efd3edc
Safa C <csafa8001@gmail.com>
2024-02-17 11:43:25 +0530

    updated client

--- client/src/components/restaurant-navbar/RestaurantNavbar.jsx
+++ client/src/components/restaurant-navbar/RestaurantNavbar.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import './restaurant-navbar.css'
 import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import User from '../../images/user.png'
+import User from '../../images/user.png';
+
 function RestaurantNavbar() {
   return (
     <div className='rest-nav-bar'>
