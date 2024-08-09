3f5d3f59458b4ed916fa65934c7607706600892b
Safa C <csafa8001@gmail.com>
2024-08-09 18:19:19 +0530

    updated cors

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/hungryhub/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://hungryhub-frontend-o1w1.onrender.com',
   credentials: true,
 }));
 app.use(express.static('uploads'));
